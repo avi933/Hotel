9f591885609b3d8a80392abf9ca8dccbd66347fb
avi993 <aviramjeeawon@gmail.com>
2022-09-16 10:03:05 +0400

    update hotel models

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    number_of_person=models.IntegerField()
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    booking_date = models.DateField()
+    additional_info = models.TextField()
+    def __str__(self) :
+        return f'{self.first_name} {self.last_name}'
+
+class Staff(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length= 20)
+    address = models.CharField(max_length=100)
+    date_join = models.DateField()
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Review(models.Model):
+    email = models.EmailField()
+    text = models.TextField()
+    def __str__(self):
+        return f'{self.email}'
+
+class Room(models.Model):
+    rt = [("std","Standard"),("de","Delux"),("sin","Single"),("db","Double")]
+    room_trype = models.CharField(max_length=3, choices = rt)
+    price = models.IntegerField()
+    status = models.BooleanField( default= False)
+
+class Vacancies(models.Model):
+    September=models.DateField()
+    October=models.DateField()
+    November=models.DateField()
+
+class Contact(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    questions=models.TextField()
+
+
+
+
+
+        
+
+
--- interface/views.py
+++ interface/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from ast import BoolOp
+from django.shortcuts import render, redirect
+from .models import Vacancies,Booking,Contact,Review
+from .forms import BookingForm,ContactForm, ReviewForm
 
 # Create your views here.
+def make_booking(request):
+    context={'bookings': Booking.objects.all()}
+    context.update({'form':BookingForm})
+
+    if request.method=='POST':
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('visitors_homepage')
+    return render(request, 'make_booking.html', context)
+
+def visitors_page(request):
+   vacancies= Vacancies.objects.all()
+   return render(request,'visitors_interface.html',{'vacancies':vacancies})
+
+def contact(request):
+    context={'contact':Contact.objects.all()}
+    context.update({'form':ContactForm})
+
+    if request.method == 'POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('visitors_homepage')
+    return render(request, 'contact_us.html', context)
+
+
+def review(request):
+    context={'contact':Review.objects.all()}
+    context.update({'form':ReviewForm})
+
+    if request.method == 'POST':
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('visitors_homepage')
+    return render(request, 'review.html', context)
+
+def show_bookings(request):
+    context={'bookings':Booking.objects.all()}
+    return render(request, 'show_booking.html', context)
+
+def show_booking_by_id(request,id:int):
+    context={'booking':Booking.objects.get(id=id)}
+    return render(request, 'show_booking_by_id.html', context)
+
+def staff_interface(request):
+    context=None
+    return render(request, 'staff_interface.html',context)
+
